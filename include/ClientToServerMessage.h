3c955d1beab9289f45d1e6b923d063449b3d0325
nivkap <84071374+nivkap@users.noreply.github.com>
2022-01-04 19:02:53 +0200

    clientToServer.h

--- include/ClientToServerMessage.h
+++ include/ClientToServerMessage.h
@@ -1,5 +1,6 @@
 using namespace std;
 #include <vector>
+#include <string>
 
 #ifndef ASSIGNMENT3_SPL_CLIENT_CLIENTTOSERVERMESSAGE_H
 #define ASSIGNMENT3_SPL_CLIENT_CLIENTTOSERVERMESSAGE_H
@@ -23,5 +24,90 @@ public:
     ClientToServerMessage(cts_message_type type);
     virtual vector<char> encode()=0;
 };
+
+class RegisterMessage : public ClientToServerMessage {
+public:
+    RegisterMessage(const vector<char> &message);
+    vector<char> encode();
+private:
+    string username;
+    string password;
+    string birthday;
+};
+
+class LoginMessage : public ClientToServerMessage {
+public:
+    LoginMessage(const vector<char> &message);
+    vector<char> encode();
+private:
+    string username;
+    string password;
+    bool captcha;
+};
+
+class LogoutMessage : public ClientToServerMessage {
+public:
+    LoginMessage(vector<char> &message, string userName);
+    vector<char> encode();
+};
+
+class FollowOrUnfollowMessage : public ClientToServerMessage {
+public:
+    FollowOrUnfollowMessage(const vector<char> &message, string userName);
+    vector<char> encode();
+private:
+    bool follow;
+    string otherUserName;
+};
+
+class PostMessage : public ClientToServerMessage {
+public:
+    PostMessage(const vector<char> &message, string userName);
+    vector<char> encode();
+private:
+    string content;
+};
+
+class PMMessage : public ClientToServerMessage {
+public:
+    PMMessage(const vector<char> &message, string userName);
+    vector<char> encode();
+private:
+    string content;
+    string sendingTimeAndDate;
+    string otherUserName;
+};
+
+class LoggedInStates : public ClientToServerMessage {
+public:
+    LoggedInStates(const vector<char> &message, string userName);
+    vector<char> encode();
+};
+
+class StatisticsMessage : public ClientToServerMessage {
+public:
+    StatisticsMessage(const vector<char> &message, string userName);
+    vector<char> encode();
+private:
+    vector<string> userNamesList;
+};
+
+class BlockMessage : public ClientToServerMessage {
+public:
+    BlockMessage(const vector<char> &message, string userName);
+    vector<char> encode();
+private:
+    string otherUserName;
+};
+
+class FetchNotificationMessage : public ClientToServerMessage {
+public:
+    FetchNotificationMessage(const vector<char> &message, string userName);
+    vector<char> encode();
+
+};
+
+
+
 
 #endif //ASSIGNMENT3_SPL_CLIENT_CLIENTTOSERVERMESSAGE_H
