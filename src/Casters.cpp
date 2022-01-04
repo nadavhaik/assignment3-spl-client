b2adef7b6ab83f72125fc5c594af46bf674f0db7
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-01-04 18:12:06 +0200

    Update Casters.cpp

--- src/Casters.cpp
+++ src/Casters.cpp
@@ -1,4 +1,7 @@
+#include <vector>
 using namespace std;
+
+
 
 short bytesToShort(char* bytesArr){
     short result = (short)((bytesArr[0] & 0xff) << 8);
@@ -6,8 +9,24 @@ short bytesToShort(char* bytesArr){
     return result;
 }
 
-void shortToBytes(short num, char* bytesArr)
-{
+void shortToBytes(short num, char* bytesArr) {
     bytesArr[0] = ((num >> 8) & 0xFF);
     bytesArr[1] = (num & 0xFF);
+}
+short byteVectorToShort(vector<char> vectorList) {
+    char *a = new char[vectorList.size()];
+    for(size_t i=0; i<vectorList.size(); i++) {
+        a[i] = vectorList[i];
+    }
+    short res = bytesToShort(a);
+    delete[] a;
+    return res;
+}
+
+vector<char> shortToBytesList(short num) {
+    vector<char> v;
+    v.push_back(((num >> 8) & 0xFF));
+    v.push_back((num & 0xFF));
+
+    return v;
 }
